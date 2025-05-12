76183f2d7d03a9cdf0f9f0f2f142430ac2741f27
Guillem Forns Sant <gforns-s@cbr3s3.42barcelona.com>
2025-05-12 08:59:40 +0200

    fixed issue:

--- Project/src/Errors.cpp
+++ Project/src/Errors.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Errors.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: josegar2 <josegar2@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 20:15:09 by josegar2          #+#    #+#             */
-/*   Updated: 2025/05/10 20:11:27 by josegar2         ###   ########.fr       */
+/*   Updated: 2025/05/12 08:22:08 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,7 @@ static std::map<std::string, std::string> createErrorFormats() {
     m.insert(std::make_pair(ERR_BADCHANMASK, "<client> <channel> :Bad Channel Mask"));
     m.insert(std::make_pair(ERR_CHANOPRIVSNEEDED, "<client> <channel> :You're not channel operator"));
 	m.insert(std::make_pair(ERR_UNKNOWNMODE, "<client> <char> :is unknown mode char to me"));
-	m.insert(std::make_pair(ERR_USERNOTINCHANNEL, "<client> <nick> <channel> :They aren't on that channel")); //need to fix 04.05 08.09 pm
+	m.insert(std::make_pair(ERR_USERNOTINCHANNEL, "<client> <nick> <channel> :They aren't on that channel")); //need to fix Still 12.05.25 08.13 am
 
     
     return m;
@@ -84,7 +84,12 @@ std::string ircErrorText(const std::string& code, const Command& cmd, const Clie
 		if (cmd.getName() == "MODE")
 			reply.replace(pos, 6, sender.get_nick());
 		else
-			reply.replace(pos, 6, args[0]);
+		{
+			if (args.size() > 1)
+				reply.replace(pos, 6, args[1]);
+			else
+				reply.replace(pos, 6, args[0]);
+		}
 	}
 	
 	// Replace <channel>
